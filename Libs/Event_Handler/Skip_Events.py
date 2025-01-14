f923b06b20c96502231208a4ce8a00cb9a135fba
Jan Vaško <50368892+JanVasko1@users.noreply.github.com>
2025-01-14 23:12:21 +0100

    Skip Event refactoring

--- Libs/Event_Handler/Skip_Events.py
+++ Libs/Event_Handler/Skip_Events.py
@@ -1,36 +1,27 @@
 # Import Libraries
 from pandas import DataFrame
 import pandas
-from datetime import datetime
 import Libs.Defaults_Lists as Defaults_Lists
 
 # ---------------------------------------------------------- Set Defaults ---------------------------------------------------------- #
 Settings = Defaults_Lists.Load_Settings()
 Day_Start_Subject = Settings["Event_Handler"]["Events"]["Start_End_Events"]["Start"]
 Day_End_Subject = Settings["Event_Handler"]["Events"]["Start_End_Events"]["End"]
-Skip_Events_list = Settings["Event_Handler"]["Events"]["Skip"]
+HomeOffice_Subject = Settings["Event_Handler"]["Events"]["Special_Events"]["HomeOffice"]["Search_Text"]
+Lunch_Subject = Settings["Event_Handler"]["Events"]["Special_Events"]["Lunch"]["Search_Text"]
+
+
+Skip_Events_list = list(Settings["Event_Handler"]["Events"]["Skip"])
 Skip_Events_list.append(Day_Start_Subject)
 Skip_Events_list.append(Day_End_Subject)
-
-Columns = ["Subject", 
-        "Start_Date", 
-        "End_Date",
-        "Start_Time", 
-        "End_Time",
-        "Duration",
-        "Project",
-        "Activity",
-        "Recurring",
-        "Busy_Status",
-        "Meeting_Room",
-        "All_Day_Event",
-        "Event_Empty_Insert",
-        "Within_Working_Hours",
-        "Location"]
+Skip_Events_list.append(HomeOffice_Subject)
+Skip_Events_list.append(Lunch_Subject)
 
 # ---------------------------------------------------------- Main Function ---------------------------------------------------------- #
 def Skip_Events(Events: DataFrame):
-    Cumulated_Events = pandas.DataFrame(columns=Columns)
+    Delete_Index_List = []
+
+    # Iterate over all lines
     for row in Events.iterrows():
         # Define current row as pandas Series
         row_Series = pandas.Series(row[1])
@@ -42,11 +33,10 @@ def Skip_Events(Events: DataFrame):
             if Part_Found == -1:
                 pass
             else:
-                break
+                Delete_Index_List.append(row[0])
 
-        if Part_Found == -1:
-            Cumulated_Events.loc[Cumulated_Events.shape[0]] = row_Series
-        else:
-            pass
+    # Delete from Dataframe
+    for Index_to_Del in Delete_Index_List:
+        Events.drop(labels=[Index_to_Del], axis=0, inplace=True)
 
-    return Cumulated_Events
+    return Events
