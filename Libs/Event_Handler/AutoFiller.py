82ff45935ee8b5f82712f4b7592d884c25c31831
Jan Vaško <50368892+JanVasko1@users.noreply.github.com>
2025-01-19 19:59:18 +0100

    Auto_Activity_Corrections v1

--- Libs/Event_Handler/AutoFiller.py
+++ Libs/Event_Handler/AutoFiller.py
@@ -5,6 +5,7 @@ import Libs.Defaults_Lists as Defaults_Lists
 # ---------------------------------------------------------- Set Defaults ---------------------------------------------------------- #
 Settings = Defaults_Lists.Load_Settings()
 Details_dict = Settings["Event_Handler"]["Events"]["Auto_Filler"]["Search_Text"]
+Activity_Correction_dict = Settings["Event_Handler"]["Events"]["Auto_Filler"]["Activity_Correction"]
 
 # ---------------------------------------------------------- Main Function ---------------------------------------------------------- #
 def AutoFiller(Events: DataFrame):
@@ -48,11 +49,21 @@ def AutoFiller(Events: DataFrame):
     return Events
 
 def Auto_Activity_Corrections(Events: DataFrame):
-    #! Dodělat --> nastavit nějaký automatický process a taky data do SEttings, který by automaticky kontrolovali povolené kombinace Projektu a Aktivity a případně opravit aktivitu + potřeba upravit nastavení a stahovat i k projektům a Aktivitám vztah na "Project Type"
-    """
-    - musí se vytvořit i nové nastavení a musí se to pokrýt i v stahování "Project/ Activity", kdy ty kombinace by měl aktualizovat
-    - pro každý záznam musí kontorlovat jestli Project a Activity patří pod stejný "Proejct Type" a když ne mapovat
-    Mapování
-        Project - Activity --> Activity
-    """
+    for row in Events.iterrows():
+        # Define current row as pandas Series
+        row_index = row[0]
+        row_Series = pandas.Series(row[1])
+        Event_Project = row_Series["Project"]
+        Event_Activity = row_Series["Activity"]
+
+        for item in Activity_Correction_dict.items():
+            Check_Project = item[1]["Project"]
+            Check_Activity = item[1]["Wrong Activity"]
+
+            if (Event_Project == Check_Project) and (Event_Activity == Check_Activity):
+                New_Activity = Check_Activity = item[1]["Correct Activity"]
+                Events.at[row_index, "Activity"] = New_Activity
+            else:
+                pass
+
     return Events
